93f15aa47f8a9567df8526d10b6eb076b864b0ca
joeldenning <joeldenning@gmail.com>
2023-05-10 15:08:44 -0600

    Self review

--- updateReadmeVersion.mjs
+++ updateReadmeVersion.mjs
@@ -1,7 +1,7 @@
 import replace from "replace-in-file";
 import packageJson from "./package.json" assert { type: "json" };
 
-console.log("Updating README");
+console.log(`Updating README to reference version ${packageJson.version}`);
 await replace({
   from: /@\d\.\d\.\d/g,
   to: `@${packageJson.version}`,
